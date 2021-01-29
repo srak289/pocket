1d512a0c756a44fd11c8efe55530b4ccd7866fcd
Spencer J. Rak <spencer.j.rak.civ@mail.mil>
2021-01-29 08:50:12 -0500

    Fixed issue in class realization

--- app/routes.py
+++ app/routes.py
@@ -26,15 +26,16 @@ app_cls = []
 def exec_cls(f):
     @wraps(f)
     def decorated_function(cls, *args, **kwargs):
+        found = False
         for clss in app_cls:
+            print(f'checking if {cls.capitalize()} == {clss.__name__}')
             if cls.capitalize() == clss.__name__:
                 cls = clss
+                found = True
                 break
-            else:
-                cls = None
-                    # should probably raise exception
-                print(f'ERROR {name} is None')
-                return redirect(url_for('index'))
+        if not found:
+            print(f'ERROR {cls} is None')
+            return redirect(url_for('index'))
         return f(cls=cls, *args, **kwargs)
     return decorated_function
 
@@ -66,65 +67,110 @@ def strip(d):
 def index():
     return render_all('index.html', content='lol')
 
-@app.route('/networks', methods=['GET', 'POST'])
-def networks():
-    if request.method == 'POST':
-        safe = strip(request.form)
-        n = Network(name=safe['name'], addr=safe['addr'])
-        print(f'Adding {n}')
-        db.session.add(n)
-        db.session.commit()
-        return redirect(url_for('networks'))
-    else:
-        networks = Network.query.all()
-        print(f'Found {networks}')
-        return render_all('networks.html', networks=networks)
+#@app.route('/networks', methods=['GET', 'POST'])
+#def networks():
+#    if request.method == 'POST':
+#        safe = strip(request.form)
+#        n = Network(name=safe['name'], addr=safe['addr'])
+#        print(f'Adding {n}')
+#        db.session.add(n)
+#        db.session.commit()
+#        return redirect(url_for('networks'))
+#    else:
+#        networks = Network.query.all()
+#        print(f'Found {networks}')
+#        return render_all('networks.html', networks=networks)
+#
+#@app.route('/network/<int:id>')
+#def network(id):
+#    n = Network.query.filter_by(id=id).one()
+#    print(f'Page for network {n}')
+#    hosts = Host.query.filter_by(network_id=n.id).all()
+#    return render_all('network.html', n=n, hosts=hosts)
+#
+#@app.route('/delete/network/<int:id>')
+#def del_network(id):
+#    n = Network.query.filter_by(id=id).one()
+#    db.session.delete(n)
+#    db.session.commit()
+#    return redirect(url_for('index'))
+#
+#@app.route('/hosts', methods=['GET', 'POST'])
+#def hosts():
+#    if request.method == 'POST':
+#        safe = strip(request.method)
+#        db.session.add(Host(name=safe['name'], addr=safe['addr']))
+#    else:
+#        hosts = Host.query.all()
+#        return render_all('hosts.html', hosts=hosts)
+#
+#@app.route('/host/<int:id>')
+#def host(id):
+#    h = Host.query.filter_by(id=id).one()
+#    ports = Port.query.filter_by(host_id=h.id).all()
+#    return render_all('host.html', host=h, ports=ports)
+#
+#@app.route('/delete/host/<int:id>')
+#def del_host(id):
+#    h = Host.query.filter_by(id=id).one()
+#    db.session.remove(n)
+#    db.session.commit()
+#    return redirect(url_for('index'))
+#
+#@app.route('/result')
+#def result():
+#    res = Network.query.all()
+#    return render_all('results.html', content=res)
+#
+#@app.route('/update/network/<int:id>')
+#def update_network(id):
+#    n = Network.query.filter_by(id=id).one()
+#    s = Scanner(n.addr)
+#    results = s.scan_network()
+#    for r in results:
+#        h = Host(network_id=n.id, addr=r['host'])
+#        db.session.add(h)
+#        for p in r['ports']:
+#            x = Port(host_id=h.id, port_num=p['port'], port_stat=p['status'])
+#            db.session.add(x)
+#    db.session.commit()
+#    return render_all('results.html', content=results)
 
-@app.route('/network/<int:id>')
-def network(id):
-    n = Network.query.filter_by(id=id).one()
-    print(f'Page for network {n}')
-    hosts = Host.query.filter_by(network_id=n.id).all()
-    return render_all('network.html', n=n, hosts=hosts)
+#@app.route('/class/<string:cls>')
+#@exec_cls
+#def test(cls):
+#    print(f'---{cls}---')
+#    if cls is not None:
+#        print(cls.query.all())
+#        return redirect(url_for('index'))
+#    else:
+#        return redirect(url_for('index'))
 
-@app.route('/delete/network/<int:id>')
-def del_network(id):
-    n = Network.query.filter_by(id=id).one()
-    db.session.delete(n)
-    db.session.commit()
-    return redirect(url_for('index'))
-
-@app.route('/hosts', methods=['GET', 'POST'])
-def hosts():
+@app.route('/many/<string:cls>')
+@exec_cls
+def read_many(cls):
     if request.method == 'POST':
         safe = strip(request.method)
-        db.session.add(Host(name=safe['name'], addr=safe['addr']))
+            # dynamically instance classes
+        #db.session.add(Host(name=safe['name'], addr=safe['addr']))
     else:
-        hosts = Host.query.all()
-        return render_all('hosts.html', hosts=hosts)
+        cs = cls.query.all()
+        for c in cs:
+            for k, v in c.__dict__.items():
+                print(f'Key {k}, Value {v}')
+        return render_all('many.html', db=db, c=c)
 
-@app.route('/host/<int:id>')
-def host(id):
-    h = Host.query.filter_by(id=id).one()
-    ports = Port.query.filter_by(host_id=h.id).all()
-    return render_all('host.html', host=h, ports=ports)
+@app.route('/<string:cls>/<int:id>')
+@exec_cls
+def read_one(cls, id):
+    c = cls.query.filter_by(id=id).one()
+    return render_all('one.html', db=db, c=c)
 
-@app.route('/delete/host/<int:id>')
-def del_host(id):
-    h = Host.query.filter_by(id=id).one()
-    db.session.remove(n)
-    db.session.commit()
-    return redirect(url_for('index'))
-
-@app.route('/result')
-def result():
-    res = Network.query.all()
-    return render_all('results.html', content=res)
-
-@app.route('/update/network/<int:id>')
-def update_network(id):
-    n = Network.query.filter_by(id=id).one()
-    s = Scanner(n.addr)
+@app.route('/update/<string:cls>/<int:id>')
+@exec_cls
+def update_one(cls, id):
+    c = cls.query.filter_by(id=id).one()
+    s = Scanner(c.addr)
     results = s.scan_network()
     for r in results:
         h = Host(network_id=n.id, addr=r['host'])
@@ -135,41 +181,10 @@ def update_network(id):
     db.session.commit()
     return render_all('results.html', content=results)
 
-@app.route('/<string:name>')
-def read_many(name):
-    if name[:-1] == 's':
-        if request.method == 'POST':
-            safe = strip(request.method)
-            db.session.add(Host(name=safe['name'], addr=safe['addr']))
-        else:
-            hosts = Host.query.all()
-            return render_all('hosts.html', hosts=hosts)
-    else:
-        pass
-
-@app.route('/class/<string:cls>')
+@app.route('/delete/<string:cls>/<int:id>')
 @exec_cls
-def test(cls):
-    print(f'---{cls}---')
-    if cls is not None:
-        print(cls.query.all())
-        return redirect(url_for('index'))
-    else:
-        return redirect(url_for('index'))
-
-@app.route('/<string:name>/<int:id>')
-def read_one(name, id):
-    h = Host.query.filter_by(id=id).one()
-    ports = Port.query.filter_by(host_id=h.id).all()
-    return render_all('host.html', host=h, ports=ports)
-
-@app.route('/update/<string:name>/<int:id>')
-def update_one(name, id):
-    pass
-
-@app.route('/delete/<string:name>/<int:id>')
-def delete_one(name, id):
-    h = Host.query.filter_by(id=id).one()
-    db.session.remove(n)
+def delete_one(cls, id):
+    c = cls.query.filter_by(id=id).one()
+    db.session.remove(c)
     db.session.commit()
-    return redirect(url_for('index'))
+    return redirect(url_for(f'/{cls.lower()}'))
